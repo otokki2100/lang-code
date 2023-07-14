b12c5995e70d3d045f42e45daf2aeb5fa15a1ea8
wayles54 <alive2100@gmail.com>
2023-07-14 10:27:57 +0900

    update

--- shell/centos-07-simple.sh
+++ shell/centos-07-simple.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-hostnamectl set-hostname centos07
+hostnamectl set-hostname centos-07
+
+touch /tmp/centos-07
 
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 
