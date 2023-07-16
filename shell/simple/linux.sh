e3821cb11dd55e432b037ee4f1705f4fd8aae608
admin-mgmt <admin-mgmt@admin-mgmts-MacBook-Air.local>
2023-07-16 21:49:07 +0900

    update

--- shell/simple/linux.sh
+++ shell/simple/linux.sh
@@ -7,5 +7,6 @@ touch /tmp/${domain}
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 
 echo "${user}:password" | chpasswd
+echo "root:password" | chpasswd
 
 systemctl restart sshd
--- shell/simple/rancheros.sh
+++ shell/simple/rancheros.sh
@@ -3,3 +3,4 @@
 touch /tmp/${domain}
 
 echo "rancher:password" | chpasswd
+echo "root:password" | chpasswd
