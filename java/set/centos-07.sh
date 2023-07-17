6dc17d266891525467197a45e5eaaa86170979ed
otokki2100 <otokki2100@gmail.com>
2023-07-17 14:03:02 +0900

    update

---
+++ java/set/centos-07.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo yum install -y java-1.7.0-openjdk-devel java-1.8.0-openjdk-devel java-11-openjdk-devel
+
+sudo alternatives --set java /usr/lib/jvm/java-11-openjdk-11.0.17.0.8-2.el7_9.x86_64/bin/java
+
+sudo alternatives --set javac /usr/lib/jvm/java-11-openjdk-11.0.17.0.8-2.el7_9.x86_64/bin/javac
