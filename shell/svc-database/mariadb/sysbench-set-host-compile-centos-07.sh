a552902b95b133df667775883468581b3a4ac13f
otokki2100 <otokki2100@gmail.com>
2023-07-21 18:33:23 +0900

    update

---
+++ shell/svc-database/mariadb/sysbench-set-host-compile-centos-07.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo dnf -y install automake libtool
+sudo dnf -y install mariadb105-devel
+
+curl -L https://github.com/akopytov/sysbench/archive/refs/tags/1.0.20.tar.gz -o 1.0.20.tar.gz 
+tar xvzf 1.0.20.tar.gz 
+cd sysbench-*
+./autogen.sh
+./configure
+make -j$(nproc)
+sudo make install
