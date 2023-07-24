fa5e90a207af4d60b063ce7e71327c1179e0fe27
otokki2100 <otokki2100@gmail.com>
2023-07-24 15:42:48 +0900

    update

---
+++ shell/svc-web/httpd/httpd-server-conf-host-mod_status-centos-07.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cat /usr/local/apache2/conf/httpd.conf | grep mod_status.so
+
+cat << EOF | sudo tee /usr/local/apache2/conf.d/mod_status.conf
+<Location /server-status>
+    SetHandler server-status
+    Require local
+</Location>
+EOF
+
+sudo -u daemon /usr/local/apache2/bin/apachectl restart
+
+curl http://127.0.0.1:8080/server-status
+# lynx http://127.0.0.1:8080/server-status
+## If you change the Apache server port, you cannot change the default port in apachectl status.
