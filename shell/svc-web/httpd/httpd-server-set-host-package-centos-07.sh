0f14f4f6bd11fa44a6452ad67da8cab38c14110e
otokki2100 <otokki2100@gmail.com>
2023-07-24 10:18:19 +0900

    update

---
+++ shell/svc-web/httpd/httpd-server-set-host-package-centos-07.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+yum install -y httpd
+# rpm -qa | grep httpd
+## httpd-tools-2.4.6-97.el7.centos.2.x86_64
+## httpd-2.4.6-97.el7.centos.2.x86_64
+systemctl enable --now httpd && systemctl status httpd
+
+httpd -V
+## Server version: Apache/2.4.6 (CentOS)
+
+echo hello | tee /var/www/html/index.html
