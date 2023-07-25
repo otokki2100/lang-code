ba9f143024021a5e3d3d17d2c5601ccecafea172
otokki2100 <otokki2100@gmail.com>
2023-07-25 16:43:23 +0900

    update

---
+++ shell/svc-web/httpd/httpd/httpd-server-set-host-compile-simple-01-centos-07.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+cat << EOF | sudo tee /etc/profile.d/custom.sh
+PATH=\$PATH:/usr/local/apache2/bin
+EOF
+source /etc/profile.d/custom.sh
+
+sudo yum -y install git gcc gcc-c++ make libtool autoconf expat-devel libxml2-devel perl-devel
+## install the basic library packages required for compile
+
+git clone -b 2.4.57 https://github.com/apache/httpd.git /tmp/httpd
+
+cd /tmp/httpd/
+
+sed -i "s/-ldl/-ldl -lexpat/g" /tmp/httpd/build/config_vars.mk
+
+./buildconf
+
+./configure --prefix=/usr/local/apache2 --enable-so --enable-mods-shared=most
+make -j$(nproc) && sudo make install
+echo $?
+
+sudo mkdir /usr/local/apache2/conf.d/
+sudo touch /usr/local/apache2/conf.d/custom.conf
+echo "IncludeOptional conf.d/*.conf" | sudo tee -a /usr/local/apache2/conf/httpd.conf
+
+sudo sed -i "s/Listen 80/Listen 8080/g" /usr/local/apache2/conf/httpd.conf
+
+sudo chown -R daemon.daemon /usr/local/apache2
+
+sudo -u daemon /usr/local/apache2/bin/apachectl start
+apachectl -V
+
+curl http://127.0.0.1:8080
