ece157cbef13c1da54403aa1b05c326cabc0e7ea
otokki2100 <otokki2100@gmail.com>
2023-07-31 13:32:55 +0900

    update

---
+++ shell/svc-web/httpd/apr/apr-server-set-host-compile-amazl-23.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo dnf -y install git
+
+git clone -b 1.7.4 https://github.com/apache/apr.git /tmp/apr
+git clone -b 1.6.3 https://github.com/apache/apr-util.git /tmp/apr-util
---
+++ shell/svc-web/httpd/apr/apr-server-set-host-compile-ubuntu-22.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt -y install git
+
+git clone -b 1.7.4 https://github.com/apache/apr.git /tmp/apr
+git clone -b 1.6.3 https://github.com/apache/apr-util.git /tmp/apr-util
