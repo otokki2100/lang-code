5fdc077d13d8fda78d6480a7873349c26d65f953
otokki2100 <otokki2100@gmail.com>
2023-08-13 13:58:28 +0900

    update

--- shell/svc-repo/nexus/nexus-client-centos-07.sh
+++ shell/svc-repo/nexus/nexus-client-centos-07.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+sudo cp -r /etc/yum.repos.d/ /etc/yum.repos.d.bak/
+sudo rm -f /etc/yum.repos.d/*
+
+cat << EOF | sudo tee /etc/yum.repos.d/yum-c07-default-base.repo
+[yum-c07-default-base]
+name=yum-c07-default-base
+baseurl=http://10.0.101.11:8081/repository/yum-c07-default-base/
+enabled=1
+gpgcheck=0
+username=admin
+password=password
+EOF
+
+cat << EOF | sudo tee /etc/yum.repos.d/yum-c07-default-extra.repo
+[yum-c07-default-extra]
+name=yum-c07-default-extra
+baseurl=http://10.0.101.11:8081/repository/yum-c07-default-extra/
+enabled=1
+gpgcheck=0
+username=admin
+password=password
+EOF
+
+sudo yum repolist
+
 sudo yum install -y java-1.7.0-openjdk-devel java-1.8.0-openjdk-devel java-11-openjdk-devel
 
 sudo alternatives --set java /usr/lib/jvm/java-11-openjdk-11.0.19.0.7-1.el7_9.x86_64/bin/java
