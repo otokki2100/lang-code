3185fc8aeaba1e3f2749ab06bd502cd5d422efd0
otokki2100 <otokki2100@gmail.com>
2023-08-17 06:12:13 +0900

    update

--- shell/lang_java/set/client/java-client-set-host-package-centos-07.sh
+++ shell/lang_java/set/client/java-client-set-host-package-centos-07.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo yum install -y java-1.7.0-openjdk-devel java-1.8.0-openjdk-devel java-11-openjdk-devel
+sudo yum -y install java-1.7.0-openjdk-devel java-1.8.0-openjdk-devel java-11-openjdk-devel
+
+curl -L https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm -o /tmp/jdk-17_linux-x64_bin.rpm
+sudo rpm -ivh /tmp/jdk-17_linux-x64_bin.rpm
 
 sudo alternatives --set java /usr/lib/jvm/java-11-openjdk-11.0.19.0.7-1.el7_9.x86_64/bin/java
 
