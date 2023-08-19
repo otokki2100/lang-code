0f25f265b80df819918b198cfe479f7c30943778
otokki2100 <otokki2100@gmail.com>
2023-08-19 19:49:36 +0900

    update

---
+++ shell/svc-sec/sonarqube/sonarqube-server-binary-simple-centos-07.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo yum -y install java-1.7.0-openjdk-devel java-1.8.0-openjdk-devel java-11-openjdk-devel
+
+sudo alternatives --set java /usr/lib/jvm/java-11-openjdk-11.0.19.0.7-1.el7_9.x86_64/bin/java
+
+sudo alternatives --set javac /usr/lib/jvm/java-11-openjdk-11.0.19.0.7-1.el7_9.x86_64/bin/javac
+
+java -version
+
+
+
+
+
+
+
+
