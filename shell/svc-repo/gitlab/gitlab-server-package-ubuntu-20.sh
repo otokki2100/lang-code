b8e163e46295617427e088ce9bacc01f74580bb8
otokki2100 <otokki2100@gmail.com>
2023-08-19 12:26:59 +0900

    update

---
+++ shell/svc-repo/gitlab/gitlab-server-package-ubuntu-20.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+user=$1
+dist=$2
+domain=$3
+
+sudo hostnamectl set-hostname ${domain}
+
+sudo apt-get update
+sudo apt-get install -y curl openssh-server ca-certificates tzdata
+
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
+
+sudo EXTERNAL_URL="https://${domain}" apt-get install gitlab-ee
+
+sudo cat /etc/gitlab/initial_root_password
